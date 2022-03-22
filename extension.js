d83723e72c9ab554039d89885d5e865b3422881c
Mukundan314 <mukundan314@gmail.com>
2022-03-22 11:40:27 +0530

    feat: register newConnections command

--- extension.js
+++ extension.js
@@ -17,7 +17,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rabbitmq.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('rabbitmq.newConnection', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
