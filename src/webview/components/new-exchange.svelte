5d47d337ca16152b050778d6acdcad7fdd2c65d7
rohinivsenthil <rohinivsenthil@gmail.com>
2022-04-04 00:11:40 +0530

    feat: add webview for add exchange

--- src/webview/components/new-exchange.svelte
+++ src/webview/components/new-exchange.svelte
@@ -1,4 +1,124 @@
 <script>
+  //   function newExchange(someArg) {
+  //     const vscode = acquireVsCodeApi();
+  //     vscode.postMessage({
+  //       type: 'new-exchnage',
+  //       someArg,
+  //     })
 </script>
 
-<main>Hello World</main>
+<main>
+  <div class="add-exchange-container">
+    <div class="add-exchange-title">
+      <i class="codicon codicon-remote icon exchange-icon" />
+      <div>Add a new exachange</div>
+    </div>
+    <div class="add-exchange">
+      <div class="add-exchange-fields">
+        <div class="add-exchange-key">Name</div>
+        <div class="add-exchange-key">Type</div>
+        <div class="add-exchange-key">Durability</div>
+        <div class="add-exchange-key">Auto delete</div>
+        <div class="add-exchange-key">Internal</div>
+        <div class="add-exchange-key">Arguments</div>
+      </div>
+      <div class="add-exchange-fields">
+        <input
+          type="text"
+          id="exchange-name"
+          class="vscode-input add-exchange-input"
+        />
+        <input
+          type="text"
+          id="exchange-type"
+          class="vscode-input add-exchange-input"
+        />
+        <input
+          type="text"
+          id="exchange-durability"
+          class="vscode-input add-exchange-input"
+        />
+        <input
+          type="text"
+          id="exchange-auto-delete"
+          class="vscode-input add-exchange-input"
+        />
+        <input
+          type="text"
+          id="exchange-internal"
+          class="vscode-input add-exchange-input"
+        />
+        <div class="add-exchange-args">
+          <input
+            type="text"
+            id="arguments-key"
+            class="vscode-input add-exchange-input"
+          />
+          <div>=</div>
+          <input
+            type="text"
+            id="arguments-value"
+            class="vscode-input add-exchange-input"
+          />
+          <select
+            name="arg-type"
+            id="arg-type"
+            class="vscode-dropdown add-exchange-input"
+          >
+            <option value="string">String</option>
+            <option value="number">Number</option>
+            <option value="boolean">Boolean</option>
+            <option value="number">Number</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <button type="button" class="add-btn vscode-button">Add Exchange</button>
+  </div>
+</main>
+
+<style>
+  @import "@vscode/codicons/dist/codicon.css";
+  @import "./vscode.css";
+
+  .add-exchange-container {
+    margin: 50px;
+  }
+  .add-exchange-title {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    margin-bottom: 24px;
+  }
+
+  .exchange-icon {
+    color: var(--vscode-terminal-ansiCyan);
+  }
+
+  .icon {
+    margin: 0 5px 0 0;
+  }
+  .add-exchange {
+    display: flex;
+    align-items: center;
+  }
+  .add-exchange-fields {
+    display: flex;
+    flex-direction: column;
+  }
+  .add-exchange-key {
+    padding: 5px;
+    margin: 5px 10px 5px 0;
+  }
+  .add-exchange-input {
+    padding: 5px;
+    margin: 5px 10px;
+  }
+  .add-exchange-args {
+    display: flex;
+    align-items: center;
+  }
+  .add-btn {
+    margin-top: 10px;
+  }
+</style>
